64323d0719743db570a117885fd7ddf59c058ff2
vaishnavisuresh588-spec <vaishnavisuresh588@gmail.com>
2025-12-06 13:32:31 +0530

    Update employee.py

--- employee.py
+++ employee.py
@@ -1,14 +1,12 @@
-import employee
-
+import pytest
 def employee_details(name, emp_id, department, salary):
     result = (
-        f"Employee Name : {name}\n"
-        f"Employee ID   : {emp_id}\n"
-        f"Department    : {department}\n"
-        f"Salary        : {salary}\n"
+        f"Employee Name: {name}\n"
+        f"Employee ID: {emp_id}\n"
+        f"Department: {department}\n"
+        f"Salary: {salary}"    
     )
     return result
-
 
 if __name__ == "__main__":
     name = "vaishnavi"
