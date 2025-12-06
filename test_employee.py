1cb3238f93ea970eb25277bdf70ba1bec1b2b610
vaishnavisuresh588-spec <vaishnavisuresh588@gmail.com>
2025-12-06 13:34:07 +0530

    Fix formatting in employee details test

--- test_employee.py
+++ test_employee.py
@@ -4,8 +4,7 @@ def test_employee_details():
     expected_output = (
         "Employee Name: vaishnavi\n"
         "Employee ID: 01fe24bca014\n"
-        "Department:bca\n"
-        "Salary:60000\n"
+        "Department: bca\n"
+        "Salary: 60000"
     )
-
-    assert employee_details("vaishnavi","01fe24bca014","bca",60000) == expected_output
+    assert employee_details("vaishnavi", "01fe24bca014", "bca", "60000") == expected_output
